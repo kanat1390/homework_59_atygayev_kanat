88de7122a52573696ec9e019129ff78fb26b29c3
iprogrammer1390 <iprogrammer1390@gmail.com>
2022-10-12 01:47:51 +0600

    Добавлена детальная view проекта

--- src/tracker/urls.py
+++ src/tracker/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProjectListView
+from .views import ProjectListView, ProjectDetailView
 
 urlpatterns = [
     path('', ProjectListView.as_view(), name='project-list'),
+    path('<int:pk>/', ProjectDetailView.as_view(), name='project-detail')
 ]
--- src/tracker/views.py
+++ src/tracker/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from tracker.models import Project
 
 class ProjectListView(ListView):
     model = Project
+    template_name = 'tracker/project_list.html'
+
+
+class ProjectDetailView(DetailView):
+    model = Project
+    template_name = 'tracker/project_detail.html'
 
 
