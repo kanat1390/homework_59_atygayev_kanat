08f9821666cb83c035e9a4543a5ccd4da1c0cf32
iprogrammer1390 <iprogrammer1390@gmail.com>
2022-10-12 00:25:07 +0600

    Изменено название приложения на tracker

--- src/tracker/apps.py
+++ src/tracker/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TasksConfig(AppConfig):
+class TrackerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tasks'
+    name = 'tracker'
