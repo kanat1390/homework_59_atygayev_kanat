72294e31618412d7d120374c6f3a89f0877c4fd2
iprogrammer1390 <iprogrammer1390@gmail.com>
2022-10-12 01:15:15 +0600

    Добавлена модель Project.Настроена связь многие ко многим для vjltkb Task с Type

---
+++ src/tracker/models/project.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Project(models.Model):
+    started_date = models.DateField()
+    finished_date = models.DateField(null=True, blank=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+        
--- src/tracker/models/task.py
+++ src/tracker/models/task.py
@@ -3,8 +3,9 @@ from django.db import models
 class Task(models.Model):
     summary = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
+    project = models.ForeignKey(to='tracker.Project', on_delete=models.CASCADE)
     status = models.ForeignKey(to='tracker.Status', on_delete=models.PROTECT)
-    types = models.ForeignKey(to='tracker.Type', on_delete=models.PROTECT)
+    types = models.ManyToManyField('tracker.Type', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
